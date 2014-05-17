4ed45bd8c8aedac6068c27e761e8e675a8386fef
Max Ogden <max@maxogden.com>
2014-05-17 17:32:04 +0200

    use changes feed + newer dat

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
+// haha
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 var Dat = require('dat');
-var mkdirp = require('mkdirp');
-var flat = require('flat-file-db');
-var read = require('./read-registry-stream');
+var request = require('request')
+var series = require('run-series')
 var through = require('through2');
 var path = require('path');
+var ChangesStream = require('changes-stream')
+var flat = require('flat-file-db')
+var log = require('single-line-log').stdout
 var optimist = require('optimist')
 	.usage('Usage: $0 [folder]')
 	.option('p', {
@@ -21,47 +26,73 @@ if (!folder) {
 	process.exit(1);
 }
 
-mkdirp.sync(folder);
-
-var db = flat.sync(path.join(folder, 'sync.db'));
-
-var dat = new Dat(folder, {serve:false}, function(err) {
+var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, function(err) {
 	if (err) throw err;
+	
+	var db = flat.sync(path.join(folder, 'sync.db'))
+	var seq = db.get('seq')
+	
+	if (seq) console.log('last seq', seq)
+	
+	// haha
+	setInterval(function() {
+		if (seq) db.put('seq', seq)
+	}, 10000)
+	
+	update()
+	
+	function update() {		
+		function restart() {
+			setTimeout(update, 1000)
+		};
 
-	var update = function() {
-		var ws = dat.createWriteStream({
-			objects: true,
-			primary: ['name']
-		});
+    var changes = new ChangesStream({
+      db: 'https://fullfatdb.npmjs.com/registry',
+      include_docs: true,
+			since: seq
+    });
+		
+		var count = 0
 
-		ws.on('error', function(err) {
-			// ignore for now...
-		});
-
-		var seq = db.get('seq');
-
-		read(seq)
+    changes
 			.pipe(through.obj(function(data, _, cb) {
-				seq = data.seq;
-				delete data.seq;
-				cb(null, data);
-			}))
-			.on('finish', function() {
-				db.put('seq', seq, function() {
-					setTimeout(update, 5 * 60 * 1000);
-				});
-			})
-			.pipe(ws);
+				var doc = data.doc
+				
+				doc.id = doc._id
+				delete doc._id
+				
+				doc.couchSeq = seq = data.seq
 
-	};
-
-	dat.init(function() {
-		update();
-		dat.serve({
-			port: process.env.PORT || argv.port
-		}, function(err, msg) {
-			if (err) throw err;
-			console.log(msg)
-		});
-	});
-});
+				dat.put(doc, function(err, latest) {
+					if (err) throw err
+					
+					var versions = Object.keys(latest.versions)
+				
+					var fns = versions.map(function(version) {
+						var tgz = latest.versions[version].dist.tarball
+						if (!tgz) return function(cb) { setImmediate(cb) }
+						return function(cb) {
+							var filename = latest.name + '-' + version + '.tgz'
+							
+			        var ws = dat.createBlobWriteStream(filename, latest, function(err, updated) {
+								if (err) return cb(err)
+								latest = updated
+								cb()
+							})
+							
+							console.log('tgz GET', tgz)
+							
+							request(tgz).pipe(ws)
+						}
+					})
+					
+					series(fns, function(err, results) {
+						if (err) console.error('GET ERR', err)
+						console.log(++count, [latest.id, latest.version])
+						cb()
+					})
+				
+				})
+		}))
+	}
+})
