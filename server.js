9411e9163c2a8936fe6a32d9b84cf5f43795b3e8
Max Ogden <max@maxogden.com>
2014-05-18 18:32:54 +0200

    log stuff

--- server.js
+++ server.js
@@ -42,16 +42,17 @@ var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, fu
   update()
   
   function update() {
-    
     console.log('creating changes stream...')
     var count = 0
-  
-    var changes = request('https://fullfatdb.npmjs.com/registry/_changes?heartbeat=30000&include_docs=true&feed=continuous&since=' + seq)
+    
+    var reqUrl = 'https://fullfatdb.npmjs.com/registry/_changes?heartbeat=30000&include_docs=true&feed=continuous&since=' + seq
+    console.log(reqUrl)
+    var changes = request(reqUrl)
     changes.pipe(split()).pipe(through.obj({highWaterMark: 20}, function(data, enc, cb) {
       data = JSON.parse(data)
       var doc = data.doc
       
-      changes.on('end', update)
+      changes.on('finish', update)
       changes.on('error', update)
     
       // dat uses .id
