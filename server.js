9efd0ec6c7c6fde3189a2b5bfd026f63cc9efbbe
Max Ogden <max@maxogden.com>
2014-05-18 18:26:47 +0200

    remove changes stream

--- server.js
+++ server.js
@@ -1,107 +1,95 @@
 #!/usr/bin/env node
 
 // haha
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-// var Dat = require('dat');
-// var request = require('request')
-// var series = require('run-series')
+var Dat = require('dat');
+var request = require('request')
+var series = require('run-series')
 var through = require('through2');
 var path = require('path');
-var ChangesStream = require('changes-stream')
+var flat = require('flat-file-db')
+var log = require('single-line-log').stdout
+var split = require('binary-split')
+var optimist = require('optimist')
+  .usage('Usage: $0 [folder]')
+  .option('p', {
+    alias: 'port',
+    describe: 'set the port to listen on'
+  });
 
+var argv = optimist.argv;
+var folder = argv._[0];
 
-// var flat = require('flat-file-db')
-// var log = require('single-line-log').stdout
-// var optimist = require('optimist')
-// .usage('Usage: $0 [folder]')
-// .option('p', {
-//   alias: 'port',
-//   describe: 'set the port to listen on'
-// });
-// 
-// var argv = optimist.argv;
-// var folder = argv._[0];
-// 
-// if (!folder) {
-//   optimist.showHelp();
-//   process.exit(1);
-// }
-// 
-// var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, function(err) {
-//   if (err) throw err;
-//   
-//   var db = flat.sync(path.join(folder, 'sync.db'))
-//   var seq = db.get('seq')
-//   
-//   if (seq) console.log('last seq', seq)
-//   
-//   // haha
-//   setInterval(function() {
-//     if (seq) db.put('seq', seq)
-//   }, 10000)
-//   
-//   update()
-//   
-//   function update() {
-//     
-//     console.log('creating changes stream...')
+if (!folder) {
+  optimist.showHelp();
+  process.exit(1);
+}
 
-
-
-   
+var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, function(err) {
+  if (err) throw err;
+  
+  var db = flat.sync(path.join(folder, 'sync.db'))
+  var seq = db.get('seq')
+  
+  if (seq) console.log('last seq', seq)
+  
+  // haha
+  setInterval(function() {
+    if (seq) db.put('seq', seq)
+  }, 10000)
+  
+  update()
+  
+  function update() {
+    
+    console.log('creating changes stream...')
     var count = 0
+  
+    var changes = request('https://fullfatdb.npmjs.com/registry/_changes?heartbeat=30000&include_docs=true&feed=continuous')
+    changes.pipe(split()).pipe(through.obj({highWaterMark: 20}, function(data, enc, cb) {
+      data = JSON.parse(data)
+      var doc = data.doc
     
-    var cstream = new ChangesStream({
-      db: 'https://fullfatdb.npmjs.com/registry',
-      include_data: true
-    })
+      // dat uses .id
+      doc.id = doc._id
+      delete doc._id
     
-    cstream.pipe(through.obj({highWaterMark: 20}, function(ch, enc, next){
-      console.log(ch)
-      setTimeout(next, 1000)
+      // keep the seq around because why not
+      doc.couchSeq = seq = data.seq
+    
+      dat.put(doc, function(err, latest) {
+        if (err) throw err
+    
+        var versions = Object.keys(latest.versions)
+    
+        // fetch all attachments
+        var fns = versions.map(function(version) {
+          var tgz = latest.versions[version].dist.tarball
+          if (!tgz) return function(cb) { setImmediate(cb) }
+          return function(cb) {
+            var filename = latest.name + '-' + version + '.tgz'
+        
+            var ws = dat.createBlobWriteStream(filename, latest, function(err, updated) {
+              if (err) return cb(err)
+              latest = updated
+              cb()
+            })
+        
+            console.log('tgz GET', tgz)
+        
+            request(tgz).pipe(ws)
+          }
+        })
+    
+        series(fns, function(err, results) {
+          if (err) console.error('GET ERR', err)
+          console.log(++count, [latest.id, latest.version])
+          cb()
+        })
+    
+      })
     }))
-      // var doc = data.doc
-   //    
-   //    // dat uses .id
-   //    doc.id = doc._id
-   //    delete doc._id
-   //    
-   //    // keep the seq around because why not
-   //    doc.couchSeq = seq = data.seq
-   //    
-   //    dat.put(doc, function(err, latest) {
-   //      if (err) throw err
-   //      
-   //      var versions = Object.keys(latest.versions)
-   //      
-   //      // fetch all attachments
-   //      var fns = versions.map(function(version) {
-   //        var tgz = latest.versions[version].dist.tarball
-   //        if (!tgz) return function(cb) { setImmediate(cb) }
-   //        return function(cb) {
-   //          var filename = latest.name + '-' + version + '.tgz'
-   //          
-   //          var ws = dat.createBlobWriteStream(filename, latest, function(err, updated) {
-   //            if (err) return cb(err)
-   //            latest = updated
-   //            cb()
-   //          })
-   //          
-   //          console.log('tgz GET', tgz)
-   //          
-   //          request(tgz).pipe(ws)
-   //        }
-   //      })
-   //      
-   //      series(fns, function(err, results) {
-   //        if (err) console.error('GET ERR', err)
-   //        console.log(++count, [latest.id, latest.version])
-   //        cb()
-   //      })
-   //      
-   //    })
-    // })
     
-//   }
-// })
+  }
+})
