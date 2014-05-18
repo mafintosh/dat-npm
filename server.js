839b3281cf94fa26189f202b05dfab78fc290b6a
Max Ogden <max@maxogden.com>
2014-05-18 18:08:17 +0200

    update

--- server.js
+++ server.js
@@ -3,47 +3,47 @@
 // haha
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-var Dat = require('dat');
-var request = require('request')
-var series = require('run-series')
+// var Dat = require('dat');
+// var request = require('request')
+// var series = require('run-series')
 var through = require('through2');
 var path = require('path');
 var ChangesStream = require('changes-stream')
-var flat = require('flat-file-db')
-var log = require('single-line-log').stdout
-var optimist = require('optimist')
-.usage('Usage: $0 [folder]')
-.option('p', {
-  alias: 'port',
-  describe: 'set the port to listen on'
-});
-
-var argv = optimist.argv;
-var folder = argv._[0];
-
-if (!folder) {
-  optimist.showHelp();
-  process.exit(1);
-}
-
-var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, function(err) {
-  if (err) throw err;
-  
-  var db = flat.sync(path.join(folder, 'sync.db'))
-  var seq = db.get('seq')
-  
-  if (seq) console.log('last seq', seq)
-  
-  // haha
-  setInterval(function() {
-    if (seq) db.put('seq', seq)
-  }, 10000)
-  
-  update()
-  
-  function update() {
-    
-    console.log('creating changes stream...')
+// var flat = require('flat-file-db')
+// var log = require('single-line-log').stdout
+// var optimist = require('optimist')
+// .usage('Usage: $0 [folder]')
+// .option('p', {
+//   alias: 'port',
+//   describe: 'set the port to listen on'
+// });
+// 
+// var argv = optimist.argv;
+// var folder = argv._[0];
+// 
+// if (!folder) {
+//   optimist.showHelp();
+//   process.exit(1);
+// }
+// 
+// var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, function(err) {
+//   if (err) throw err;
+//   
+//   var db = flat.sync(path.join(folder, 'sync.db'))
+//   var seq = db.get('seq')
+//   
+//   if (seq) console.log('last seq', seq)
+//   
+//   // haha
+//   setInterval(function() {
+//     if (seq) db.put('seq', seq)
+//   }, 10000)
+//   
+//   update()
+//   
+//   function update() {
+//     
+//     console.log('creating changes stream...')
 
     var changes = new ChangesStream({
       db: 'https://fullfatdb.npmjs.com/registry',
@@ -98,5 +98,5 @@ var dat = new Dat(folder, {port: process.env.PORT || argv.port, serve: true}, fu
     })
     
     changes.pipe(fetcher)
-  }
-})
+//   }
+// })
