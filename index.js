f732365a16a4e726b15b8a4deb9a91d12c0ac21a
Mathias Buus <mathiasbuus@gmail.com>
2014-08-17 16:47:58 +0200

    rewrite

--- index.js
+++ index.js
@@ -1,105 +1,136 @@
-var request = require('request');
-var runSeries = require('run-series');
-var through = require('through2');
-var path = require('path');
-var flat = require('flat-file-db');
-var log = require('single-line-log').stdout;
-var split = require('binary-split');
+var request = require('request')
+var through = require('through2')
+var split = require('binary-split')
+var once = require('once')
+var pump = require('pump')
+var concat = require('concat-stream')
+
+var tick = function(fn, err, val) {
+  process.nextTick(function() {
+    fn(err, val)
+  })
+}
+
+var latestSeq = function(dat, ready) {
+  // 10 just to make sure - pretty sure we only need 2 (1 in case we hit a schema)
+  dat.createChangesReadStream({data:true, decode:true, tail:10}).pipe(concat(function(changes) {
+    if (!changes.length) return ready(0)
+
+    var seq = changes
+      .map(function(change) {
+        return (change.value && change.value.couchSeq) || 0
+      })
+      .reduce(function(a, b) {
+        return Math.max(a, b)
+      })
+
+    ready(seq)
+  }))
+}
+
+var log = function(fmt) {
+  fmt = '[dat-npm] '+fmt
+  console.log.apply(console, arguments)
+}
 
 module.exports = function(dat, cb) {
-  cb()
-  var db = flat.sync('./sync.db');
-  var seq = db.get('seq');
-  
-  if (seq) console.log('last seq', seq);
-  
-  // haha
-  setInterval(function() {
-    if (seq) db.put('seq', seq);
-  }, 10000);
-  
-  update();
-  
-  function update() {
-    console.log('creating changes stream...');
-    var count = 0;
-    
-    var reqUrl = 'https://skimdb.npmjs.com/registry/_changes?heartbeat=30000&include_docs=true&feed=continuous' + (seq ? '&since=' + seq : '');
-    console.log(reqUrl);
-    var changes = request(reqUrl);
-    
-    changes.pipe(split()).pipe(through.obj({highWaterMark: 20}, function(data, enc, cb) {
-      data = JSON.parse(data);
-      var doc = data.doc;
-      
-      changes.on('finish', update);
-      changes.on('error', update);
-      
-      // dat uses .key
-      doc.key = doc._id;
-      delete doc._id;
-      
-      // dat reserves .version, and .version shouldn't be on top level of npm docs anyway
-      delete doc.version
-      
-      // keep the seq around because why not
-      doc.couchSeq = seq = data.seq;
-      
-      dat.get(doc.id, function(err, existing) {
-        if (err) return put();
-        getAttachments(existing);
-      })
-      
-      function put() {
-        dat.put(doc, function(err, latest) {
-          if (err) {
-            console.error('PUT ERR!', doc, err);
-            return cb();
-          }
-          getAttachments(latest);
-        });
+  latestSeq(dat, function(seq) {
+    seq = Math.max(0, seq-1) // sub 1 incase of errors
+
+    if (seq) log('Continuing fetching npm data from seq: %d', seq)
+
+    var url = 'https://skimdb.npmjs.com/registry/_changes?heartbeat=30000&include_docs=true&feed=continuous' + (seq ? '&since=' + seq : '');
+
+    var getAttachments = function(latest, cb) {
+      if (!latest.versions) return cb(null, latest)
+      var versions = Object.keys(latest.versions)
+
+      var loop = function(err, latest) {
+        if (err) return cb(err)
+
+        var version = versions.shift()
+        if (!version) return cb(null, latest)
+
+        var filename = latest.name + '-' + version + '.tgz';
+        var tgz = latest.versions[version].dist.tarball
+
+        if (!tgz) {
+          log('No dist.tarball available for %s (%s)', latest.name, version)
+          return tick(loop, null, latest)
+        }
+
+        if (latest.blobs && latest.blobs[filename]) return tick(loop, null, latest)
+
+        var next = once(loop)
+        var ws = dat.createBlobWriteStream(filename, latest, next)
+
+        log('Downloading %s', tgz)
+
+        pump(request(tgz), ws, function(err) {
+          if (err) next(err)
+        })
       }
-      
-      function getAttachments(latest) {
-        if (!latest.versions) return cb()
-        var versions = Object.keys(latest.versions);
-      
-        // fetch all attachments
-        var fns = [];
-        versions.map(function(version) {
-          var filename = latest.name + '-' + version + '.tgz';
-          var tgz = latest.versions[version].dist.tarball;
-          if (!tgz) return console.log(latest.name, version, 'has no dist.tarball');
-          if (latest.attachments && latest.attachments[filename]) return console.log(filename, 'already in doc');
-          
-          fns.push(getAttachment);
-          
-          function getAttachment(cb) {
-          
-            var ws = dat.createBlobWriteStream(filename, latest, function(err, updated) {
-              if (err) return cb(err);
-              latest = updated;
-              cb();
-            })
-          
-            console.log('tgz GET', tgz);
-            var req = request(tgz);
-            req.on('error', function(err) {
-              console.error('ERROR fetching', tgz, err);
-              cb();
-            })
-            req.pipe(ws);
-          }
+
+      loop(null, latest)
+    }
+
+    var update = function(err) {
+      if (err) {
+        log('Error: %s - retrying in 5s', err.message)
+        return setTimeout(update, 5000)
+      }
+
+      var write = function(data, enc, cb) {
+        data = JSON.parse(data)
+        var doc = data.doc
+
+        log('Updating %s (seq: %d)', doc._id, data.seq)
+
+        var ondone = function(err, doc) {
+          if (err) return cb(err)
+          log('Updated %s (version: %d)', doc.key, doc.version)
+          cb()
+        }
+
+        // dat uses .key
+        doc.key = doc._id
+        delete doc._id
+
+        // dat reserves .version, and .version shouldn't be on top level of npm docs anyway
+        delete doc.version
+
+        // keep the seq around because why not
+        doc.couchSeq = data.seq
+
+        var put = function(doc) {
+          dat.put(doc, {version:doc.version}, function(err, doc) {
+            if (err) return ondone(err)
+            getAttachments(doc, ondone)
+          })
+        }
+
+        dat.get(doc.key, function(err, existing) {
+          if (err && !err.notFound) return ondone(err)
+
+          if (!existing) return put(doc)
+
+          log('Previous version for %s (version: %d) found. Updating...', doc.key, existing.version)
+          doc.blobs = existing.blobs
+          doc.version = existing.version
+
+          // ensure that the old blobs we're fetched (to survive a restart)
+          getAttachments(doc, function(err, doc) {
+            if (err) return ondone(err)
+            put(doc)
+          })
         })
-      
-        runSeries(fns, function(err, results) {
-          if (err) console.error('GET ERROR!', err);
-          console.log(++count, [latest.id, latest.version]);
-          cb();
-        })
-      };
-       
-    }));
-    
-  };
+      }
+
+      pump(request(url), split(), through.obj(write), update)
+    }
+
+    update()
+  })
+
+  cb()
 }
