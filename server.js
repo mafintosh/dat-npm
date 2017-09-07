7c96d1a877aefd5c91c2464b6f2a5eed240e790d
Max Ogden <max@maxogden.com>
2017-09-07 10:47:07 -0700

    remove server

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-var http = require('http')
-var server = require('./rest-api.js')(function (err, router, datNpm) {
-  var server = http.createServer(function (req, res) {
-    console.error(req.method, req.url)
-    router(req, res)
-  })
-  var port = process.env.PORT || 8080
-  server.listen(port, function () {
-    console.log('listening on', port)
-    console.log('Sharing hypercores', {
-      tarballs: datNpm.tarballs.key.toString('hex'), 
-      meta: datNpm.meta.key.toString('hex')
-    })
-    datNpm.startUpdating()
-  })
+var DatNPM = require('./index.js')
+DatNPM(function (err, datNpm) {
+  datNpm.startUpdating()
 })
