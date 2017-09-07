de5157d34687f1b1ab811ad3f87cba54e0000f71
Max Ogden <max@maxogden.com>
2017-09-07 10:48:54 -0700

    missing dep

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var DatNPM = require('./index.js')
 DatNPM(function (err, datNpm) {
+  console.log('Sharing hypercores', {		
+    tarballs: datNpm.tarballs.key.toString('hex'), 		
+    meta: datNpm.meta.key.toString('hex')		
+  })
   datNpm.startUpdating()
 })
